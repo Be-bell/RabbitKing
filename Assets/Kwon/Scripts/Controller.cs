ba1755e325790edc27a94e366fdf140b39f46ba2
KwonSinwook <1005good@naver.com>
2024-06-20 18:53:04 +0900

    Add > Player anim & Player Jump
    토끼 애니매이션을 입힌 플레이어와 점프 구현
    (앞으로 가면서 아주 짧은 점프 연타하면 캐릭터 안움직임)

--- Assets/Kwon/Scripts/Controller.cs
+++ Assets/Kwon/Scripts/Controller.cs
@@ -1,11 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Controller : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private SpriteRenderer renderer;
+    private Animator animator;
 
     public int jumpPower = 5;
     public int moveSpeed = 2;
@@ -16,17 +16,21 @@ public class Controller : MonoBehaviour
     public bool isGround = false;
     public bool isJumping = false;
 
+    public bool standing = false;
+    public float stand = 0f;
+    private bool flag = false;
+
     private Vector2 value;
-    private Vector2 tmp;
 
     public float timer = 0f;
+
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        renderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
-
-
 
     private void Update()
     {
@@ -34,32 +38,82 @@ public class Controller : MonoBehaviour
         {
             timer += Time.deltaTime;
         }
-    }
 
+        if (standing)
+        {
+            stand += Time.deltaTime;
+
+            if (stand >= 2f)
+            {
+                animator.SetBool("IsDown", false);
+                standing = false;
+                stand = 0f;
+            }
+        }
+
+    }
 
     private void FixedUpdate()
     {
-        if (!isCharging && isGround && !isJumping)
+
+
+        if (!isCharging && isGround && !isJumping && !animator.GetBool("IsDown"))
         {
             rb.velocity = new Vector2(value.x * moveSpeed, rb.velocity.y);
+            animator.SetBool("IsDown", false);
+
+            if (rb.velocity.x != 0)
+            {
+                animator.SetBool("IsIdle", false);
+                animator.SetBool("IsWalk", true);
+            }
+
+            else
+            {
+                animator.SetBool("IsIdle", true);
+                animator.SetBool("IsWalk", false);
+            }
         }
+
+        if (rb.velocity.y < 0)
+        {
+            animator.SetBool("IsFall", true);
+        }
+
+        else animator.SetBool("IsFall", false);
+
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         value = context.ReadValue<Vector2>();
+        if (!isCharging && isGround && !isJumping && !standing && context.action.phase == InputActionPhase.Started)
+        {
+            renderer.flipX = value.x == 1f;
+        }
     }
 
     public void OnJump(InputAction.CallbackContext context)
     {
+
+        if (isJumping)
+        {
+            return;
+        }
+
         if (isGround)
         {
+            animator.SetBool("IsCharging", true);
+            animator.SetBool("IsIdle", false);
+            animator.SetBool("IsWalk", false);
             isCharging = true;
             rb.velocity = new Vector2(0, rb.velocity.y);
 
             if (context.action.phase == InputActionPhase.Canceled || context.action.phase == InputActionPhase.Performed)
             {
+                animator.SetBool("IsCharging", false);
                 Jump();
+                animator.SetBool("IsJump", true);
             }
         }
     }
@@ -67,13 +121,17 @@ public class Controller : MonoBehaviour
 
     public void Jump()
     {
-        tmp = Vector2.right * value.x * xSpeed + Vector2.up * jumpPower * (timer > 1f ? 1f : timer);
-        rb.AddForce(tmp, ForceMode2D.Impulse);
+        Vector2 test = Vector2.right * value.x * xSpeed + Vector2.up * jumpPower * (timer > 1f ? 1f : timer);
+
+        flag = test.y >= 10f;
+
+        rb.AddForce(test, ForceMode2D.Impulse);
+
 
         isJumping = true;
         isCharging = false;
 
-        Debug.Log(rb.velocity); // È®ÀÎ¿ë 
+        Debug.Log(rb.velocity);
         timer = 0f;
     }
 
@@ -84,6 +142,13 @@ public class Controller : MonoBehaviour
         {
             isGround = true;
             isJumping = false;
+            animator.SetBool("IsJump", false);
+
+            if (flag)
+            {
+                animator.SetBool("IsDown", true);
+                standing = true;
+            }
         }
     }
 
