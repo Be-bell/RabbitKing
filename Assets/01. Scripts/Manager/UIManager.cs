4deb502bfe8e2f30aa55882863fd1ee972f25e51
Be-bell <whdghk1346@khu.ac.kr>
2024-06-20 23:50:21 +0900

    Add > Popup UI Input
    PopupUI 프레임워크 수정 및 Input 추가(ESC키 누르면 MenuUI 생성)

---
+++ Assets/01. Scripts/Manager/UIManager.cs
@@ -0,0 +1,129 @@
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class UIManager : MonoBehaviour
+{
+    //int _order = -10;
+
+    Stack<Popup_UI> _popupStack = new Stack<Popup_UI>();
+
+    static UIManager instance;
+    readonly string path = "Assets/03. Prefabs/UI/";
+    public static UIManager Instance {  get { return instance; } }
+
+    private void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(instance);
+        SetEventSystem();
+    }
+
+    public GameObject Root
+    {
+        get
+        {
+            GameObject root = GameObject.Find("@UI_Root");
+            if (root == null)
+                root = new GameObject { name = "@UI_Root" };
+            return root;
+        }
+    }
+
+    //public void SetCanvas(GameObject go, bool sort = true)
+    //{
+    //    Canvas canvas = Util.GetOrAddComponent<Canvas>(go);
+    //    canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+    //    canvas.overrideSorting = true;
+
+    //    if (sort)
+    //    {
+    //        canvas.sortingOrder = _order;
+    //        _order++;
+    //    }
+    //    else
+    //    {
+    //        canvas.sortingOrder = -20;
+    //    }
+    //}
+
+    private void SetEventSystem()
+    {
+        Object obj = FindObjectOfType<EventSystem>();
+        if (obj == null) 
+        {
+            Instantiate(AssetDatabase.LoadAssetAtPath<GameObject>(path + "@EventSystem.prefab"));
+        }
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Exit");
+
+        if(_popupStack.Count <= 0)
+        {
+            Debug.LogError("There is No popup UI in stack!");
+            return;
+        }
+
+        GameObject go = _popupStack.Pop().gameObject;
+        if (go != null)
+        {
+            go.SetActive(false);
+            Destroy(go);
+        }
+
+        if (_popupStack.Count > 0)
+        {
+            go = _popupStack.Peek().gameObject;
+            go.SetActive(true);
+        }
+
+    }
+
+    public void PopUp<T>() where T : Popup_UI
+    {
+        GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path + $"Popup/{typeof(T).Name}.prefab");
+
+        if (prefab == null)
+        {
+            Debug.LogError("There is no PopupUI in the path. Please check your UI name or UI class.");
+            return;
+        }
+        GameObject go;
+
+        if (_popupStack.Count > 0)
+        {
+            go = _popupStack.Peek().gameObject;
+            go.SetActive(false);
+        }
+
+        go = Instantiate(prefab, Root.transform);
+        go.SetActive(true);
+        _popupStack.Push(go.GetComponent<T>());
+    }
+
+    public void MenuUI(InputAction.CallbackContext context)
+    {
+        if(context.started)
+        {
+            if (_popupStack.Count == 0)
+            {
+                PopUp<MenuUI>();
+            }
+            else
+            {
+                while (_popupStack.Count > 0)
+                {
+                    Exit();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/01. Scripts/UI/MenuUI.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUI : Popup_UI
+{
+    private readonly string info = "Info";
+    private readonly string resume = "Resume";
+    private readonly string title = "Title";
+
+    protected override void Start()
+    {
+        base.Start();
+
+        ButtonSelect(info).onClick.AddListener(Info);
+        ButtonSelect(resume).onClick.AddListener(Resume);
+        ButtonSelect(title).onClick.AddListener(Title);
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1.0f;
+        UIManager.Instance.Exit();
+    }
+
+    public void Title()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene(0);
+    }
+
+    public void Info()
+    {
+        UIManager.Instance.PopUp<InfoUI>();
+    }
+}
