9de9ac1fcbb30e0a3d756cae752f429f4fe4cdc0
Be-bell <whdghk1346@khu.ac.kr>
2024-06-24 20:00:24 +0900

    Add > SoundManager
    SoundManager 추가 및 효과음, 배경음 추가

--- Assets/01. Scripts/Manager/SoundManager.cs
+++ Assets/01. Scripts/Manager/SoundManager.cs
@@ -1,5 +1,21 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
+
+//AudioSources Index
+public enum AudioSourceIndex
+{
+    BGM,
+    EFFECT,
+    BUTTON
+}
+
+public enum BGMIndex
+{
+    BG1,
+    BG2
+}
 
 public class SoundManager : MonoBehaviour
 {
@@ -7,8 +23,11 @@ public class SoundManager : MonoBehaviour
     static SoundManager instance;
     readonly string path = "Assets/03. Prefabs/Sound/";
 
-    AudioSource[] audioSources;
+    List<AudioSource> audioSources = new List<AudioSource>();
+    Dictionary<EffectSoundTag, AudioClip> effectSoundMap = new Dictionary<EffectSoundTag, AudioClip>();
     SoundObject soundObject;
+
+    AudioSource source;
 
     private void Start()
     {
@@ -19,13 +38,30 @@ public class SoundManager : MonoBehaviour
 
         DontDestroyOnLoad(instance);
 
-        audioSources = GetComponents<AudioSource>();
+        Init();
+
+        PlayBGM(BGMIndex.BG1);
+
+    }
+
+    private void Init()
+    {
         soundObject = AssetDatabase.LoadAssetAtPath<SoundObject>(path + "SoundObject.asset");
+
+        for (int i = 0; i < System.Enum.GetValues(typeof(AudioSourceIndex)).Length; i++)
+        {
+            audioSources.Add(Util.GetOrAddComponent<AudioSource>(gameObject));
+        }
+
+        for(int i=0; i<soundObject.effectSounds.Count; i++)
+        {
+            effectSoundMap.Add(soundObject.effectSounds[i].EffectTag, soundObject.effectSounds[i].Sound);
+        }
     }
 
     public void PlayButton()
     {
-        AudioSource button = audioSources[(int)AudioSourceIndex.BUTTON];
+        source = audioSources[(int)AudioSourceIndex.BUTTON];
 
         if (soundObject.buttonSound == null)
         {
@@ -33,14 +69,45 @@ public class SoundManager : MonoBehaviour
             return;
         }
 
-        button.PlayOneShot(soundObject.buttonSound);
+        source.PlayOneShot(soundObject.buttonSound);
         
     }
-}
 
-//AudioSources Index
-public enum AudioSourceIndex
-{
-    BUTTON,
-    BG
+    public void PlayBGM(BGMIndex idx)
+    {
+        source = audioSources[(int) AudioSourceIndex.BGM];
+
+        AudioClip clip = null;
+        switch(idx)
+        {
+            case BGMIndex.BG1:
+                clip = soundObject.BGM1;
+                break;
+            case BGMIndex.BG2:
+                clip = soundObject.BGM2;
+                break;
+        }
+
+        if(clip == null)
+        {
+            Debug.Log("There is No Audio Clip.");
+            return;
+        }
+
+        source.loop = true;
+        source.clip = clip;
+        source.volume = 0.1f;
+        source.Play();
+    }
+
+    public void PlayEffectSound(EffectSoundTag tag)
+    {
+        source = audioSources[(int)AudioSourceIndex.EFFECT];
+
+        if (!effectSoundMap.ContainsKey(tag) ||effectSoundMap[tag] == null) return;
+
+        AudioClip clip = effectSoundMap[tag];
+
+        source.PlayOneShot(clip);
+    }
 }
--- Assets/01. Scripts/SoundObject.cs
+++ Assets/01. Scripts/SoundObject.cs
@@ -1,9 +1,31 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "SoundObject", menuName = "ScriptableObject/Sound", order = int.MaxValue)]
 public class SoundObject : ScriptableObject
 {
+    [Header("Button Sound")]
     public AudioClip buttonSound;
-    public AudioClip BGSound1;
-    public AudioClip BGSound2;
+
+    [Header("BackGround Music")]
+    public AudioClip BGM1;
+    public AudioClip BGM2;
+
+    [Header("Effect Sounds")]
+    public List<EffectSound> effectSounds;
+}
+
+[Serializable]
+public class EffectSound
+{
+    public EffectSoundTag EffectTag;
+    public AudioClip Sound;
+}
+
+public enum EffectSoundTag
+{
+    CHARGE,
+    JUMP,
+    LAND
 }
